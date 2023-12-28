459a478f9e0ca1645730372559ef0b051074f146
adstep <stephensonadamj@gmail.com>
2023-12-28 13:01:41 -0800

    enforce running server setup as root

--- server/setup.sh
+++ server/setup.sh
@@ -1,3 +1,8 @@
+if [[ $(/usr/bin/id -u) -ne 0 ]]; then
+    echo "Not running as root"
+    exit
+fi
+
 install -m 755 ./attach-stream-deck.sh /usr/local/bin/attach-stream-deck.sh
 install -m 755 ./detach-stream-deck.sh /usr/local/bin/detach-stream-deck.sh
 
